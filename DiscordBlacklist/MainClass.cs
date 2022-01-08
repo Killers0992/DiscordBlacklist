1ebf7c142134b76cb3f7173727e64647bee3a83f
Killers0992 <killers0992@gmail.com>
2022-01-08 09:22:59 +0100

    Add project files.

---
+++ DiscordBlacklist/MainClass.cs
@@ -0,0 +1,45 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBlacklist
+{
+    public class MainClass : Plugin<PluginConfig>
+    {
+        public override string Name { get; } = "DiscordBlacklist";
+        public override string Author { get; } = "Killers0992";
+        public override string Prefix { get; } = "discordblacklist";
+
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.PreAuthenticating += Player_PreAuthenticating;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.PreAuthenticating -= Player_PreAuthenticating;
+            base.OnDisabled();
+        }
+
+        private void Player_PreAuthenticating(Exiled.Events.EventArgs.PreAuthenticatingEventArgs ev)
+        {
+            if (ev.UserId.EndsWith("@discord"))
+            {
+                var centralAuth = (CentralAuthPreauthFlags)ev.Flags;
+                if (centralAuth == CentralAuthPreauthFlags.NorthwoodStaff)
+                    return;
+
+                if (Config.WhitelistDiscordIds.Contains(ev.UserId))
+                    return;
+
+                ev.Reject(RejectionReason.Custom, true, Config.RejectMessage);
+            }
+        }
+    }
+}
---
+++ DiscordBlacklist/PluginConfig.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBlacklist
+{
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public List<string> WhitelistDiscordIds { get; set; } = new List<string>();
+
+        public string RejectMessage { get; set; } = "You cant use discord to join that server!";
+    }
+}
